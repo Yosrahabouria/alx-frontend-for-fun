2d57d9ff490857cb60343f2cb99d68d89ce48ae3
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-07-13 13:03:35 +0200

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,72 +1,65 @@
 #!/usr/bin/python3
-"""This script takes
-    first argument: markdown file
-    second argument: html file"""
+"""This script converts a Markdown file to an HTML file.
+First argument: markdown file
+Second argument: html file"""
 
-from sys import argv, stderr
+from sys import argv, stderr, exit
 import os
+import html
+
+
+def convert_markdown_to_html(md_lines):
+    html_lines = []
+    unordered_list = []
+    ordered_list = []
+    paragraphs = []
+
+    for line in md_lines:
+        line = line.rstrip()
+        if line.startswith('#'):
+            level = line.count('#')
+            html_lines.append('<h{}>{}</h{}>'.format(level,
+                                                     html.escape(line[level+1:].strip()),
+                                                     level))
+        elif line.startswith('- '):
+            unordered_list.append('<li>{}</li>'.format(html.escape(line[2:].strip())))
+        elif line.startswith('* '):
+            ordered_list.append('<li>{}</li>'.format(html.escape(line[2:].strip())))
+        else:
+            paragraphs.append(line)
+
+    if unordered_list:
+        html_lines.append('<ul>')
+        html_lines.extend(unordered_list)
+        html_lines.append('</ul>')
+
+    if ordered_list:
+        html_lines.append('<ol>')
+        html_lines.extend(ordered_list)
+        html_lines.append('</ol>')
+
+    if paragraphs:
+        paragraph_text = "\n".join(paragraphs).strip()
+        if paragraph_text:
+            html_lines.append('<p>{}</p>'.format(html.escape(paragraph_text).replace('\n', '<br />')))
+
+    return html_lines
 
 
 if __name__ == "__main__":
-    if (len(argv) <= 2):
-        print ('Usage: ./markdown2html.py README.md README.html', file=stderr)
+    if len(argv) <= 2:
+        print('Usage: ./markdown2html.py README.md README.html', file=stderr)
         exit(1)
-    elif (not os.path.exists(argv[1])):
-        print ('Missing {}'.format(argv[1]), file=stderr)
+    elif not os.path.exists(argv[1]):
+        print('Missing {}'.format(argv[1]), file=stderr)
         exit(1)
     else:
         with open(argv[1], 'r') as md_file:
             lines = md_file.readlines()
-        Li = []
-        unordered_list = []
-        ordered_list = []
-        paragraphs = []
-        p = []
-        for line in lines:
-            if line.startswith('#'):
-                level = line.count('#')
-                Li.append('<h{}>{}</h{}>'.format(level,
-                                                line[level+1:],
-                                                level))
-            elif line.startswith('- '):
-                unordered_list.append('<li>{}</li>'.format(line[2:-1]))
-            elif line.startswith('* '):
-                ordered_list.append('<li>{}</li>'.format(line[2:-1]))
-            else:
-                paragraphs.append(line)
-                a = "".join(paragraphs)
-                p = a.split('\n\n')
 
-        if unordered_list != []:
-            Li.append('<ul>')
-            for elm in unordered_list:
-                Li.append('\t'+elm)
-            Li.append('</ul>')
+        html_content = convert_markdown_to_html(lines)
 
-        if ordered_list != []:
-            Li.append('<ol>')
-            for elm in ordered_list:
-                Li.append('\t'+elm)
-            Li.append('</ol>')
-
-        if p != []:
-            for item in p:
-                if item != '':
-                    if '\n' in item.strip():
-                        item = item.replace('\n', '<br />')
-                    Li.append('<p>')
-                    Li.append('\t'+item.strip())
-                    Li.append('</p>')
-
-        for i, item in enumerate(L):
-            if "**" in item:
-                b = item.split("**")
-                Li[i] = item.replace("**{}**".format(b[1]),
-                                    "<b>{}</b>".format(b[1]))
-            if "__" in item:
-                b = item.split("__")
-                Li[i] = item.replace("__{}__".format(b[1]),
-                                    "<em>{}</em>".format(b[1]))
         with open(argv[2], 'w') as html_file:
-            html_file.writelines('\n'.join(Li))
+            html_file.writelines('\n'.join(html_content))
+
         exit(0)
